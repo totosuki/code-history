59fd2ccda5a71a1dd55b1adbebc4d5e88a9cb958
ueda ryusin <uedaryusin@ueda.local>
2023-05-16 14:53:16 +0900

    oranda first commit

---
+++ oranda/Python/2023-05-15a = int(input()).py
@@ -0,0 +1,10 @@
+N, C = map(int, input().split())
+
+array = []
+for i in range(N):
+    array.append(list(map(int, input().split())))
+
+
+print(" 〜 〜 〜 〜 〜 〜 〜 〜 〜 〜 〜 〜 〜 〜 〜 〜 ")
+print(array)
+
