21b527c5963cbdafc24f2c56012affab57fe6608
toripppppy <Lundy11@icloud.com>
2023-05-22 23:40:29 +0900

    回答を見て書き直してACgit status

--- AtCoder/toripppppy/AGC/062_A.py
+++ AtCoder/toripppppy/AGC/062_A.py
@@ -1,29 +1,18 @@
+from inputter import inp
 import re
- 
+
 # テストケースの数
-T = int(input())
- 
-def f(_, s):
-    # _: 文字数, s: 文字列
-    s2 = s[:-1]
-    a_list = [m.start() for m in re.finditer('A', s2)]
-    b_list = [i for i in range(len(s2)) if i not in a_list]
- 
-    s3 = s[1:]
- 
-    result = ""
- 
-    for i in a_list + b_list:
-        result += s3[i]
- 
-    return result
- 
- 
+T = int(inp())
+
+def f(_, s: str):
+    if s[-1] == "B":
+        if not re.search("BA", s):
+            return "B"
+        
+    return "A"
+
 for _ in range(T):
-    N = int(input())
-    S = input()
- 
-    while len(S) > 1:
-        S = f(N, S)
- 
-    print(S)
+    N = int(inp())
+    S = inp()
+
+    print(f(N, S))
