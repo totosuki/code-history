df5f1f66ad93bcce7d8d64b2ed588e40aabf60c3
totosuki <e231kari@gmail.com>
2023-05-27 01:23:46 +0900

    staren.pyを更新（笑）

--- totosuki/Python/etc/23_02_23_staren.py
+++ totosuki/Python/etc/23_02_23_staren.py
@@ -2,12 +2,13 @@
 
 import pyautogui
 
-count = 100 #スタンプ連打の個数
+count = 1000 #スタンプ連打の個数
 speed = 0.05 #スタンプ連打の速さ(秒)
 
 homePos = [1410, 875]
-stanpPos = [510, 310]
+stanpPos = [725, 310]
 
 pyautogui.click(homePos[0], homePos[1], interval = 1, button = "left")
+pyautogui.moveTo(725,310)
 for i in range(count):
-  pyautogui.click(stanpPos[0], stanpPos[1], interval = speed, button = "left")
+  pyautogui.click(stanpPos[0], stanpPos[1], interval = 0, button = "left")
