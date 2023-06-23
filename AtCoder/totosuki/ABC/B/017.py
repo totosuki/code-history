45681d2e1e359856742db6c9024126fdfd154f28
totosuki <e231kari@gmail.com>
2023-06-24 00:12:32 +0900

    ABC 017 Bをワンライナー化 with Lundy

--- AtCoder/totosuki/ABC/B/017.py
+++ AtCoder/totosuki/ABC/B/017.py
@@ -1,9 +1,16 @@
-# [print(x) for S in [["ch","o","k","u"]] for t in [input()] for x in list(list(map(lambda s:t.replace(s,""),S)))]
+[[l.append(l[-1].replace(s,""))if s!=""else print("YNEOS"[l[-1]!=""::2])for s in ["ch","o","k","u",""]]for l in [[input()]]]
+[[l.append(l[-1].replace(s,""))if s!=0 else print("YNEOS"[l[-1]!=""::2])for s in ["ch","o","k","u",0]]for l in[[input()]]]
+[[l.append(l[-1].replace(s,""))if s else print("YNEOS"[l[-1]!=""::2])for s in ["ch","o","k","u",0]]for l in[[input()]]]
+for l in[[input()]]:[l.append(l[-1].replace(s,""))if s else print("YNEOS"[l[-1]!=""::2])for s in ["ch","o","k","u",0]]
 
-x = input()
-for c in ["ch","o","k","u"]:
-  x = x.replace(c, "")
-print("YES") if not x else print("NO")
+for l in[input().translate(str.maketrans("","","oku"))]:print("YNEOS"[l.replace("ch","")!=""::2])
+
+# import re;print("YNEOS"[re.sub(r"ch|o|k|u", "", input())!=""::2])
+
+# x = input()
+# for c in ["ch","o","k","u"]:
+#   x = x.replace(c, "")
+# print("YES") if not x else print("NO")
 
 # if not X: print("YNEOS"[bool(x)::2]) 
 #   print("YES")
