3e3e6b68fd66c8f6c0446d08bb85b0dd176b6a92
totosuki <e231kari@gmail.com>
2023-06-25 23:44:47 +0900

    ABC 030~034 Bを追加

--- AtCoder/totosuki/ABC/B/030.py
+++ AtCoder/totosuki/ABC/B/030.py
@@ -2,9 +2,10 @@ h, m = map(int, input().split())
 h %= 12
 h_angle = (30 * h) + (30 * (m / 60))
 m_angle = 6 * m
-if abs(h_angle - m_angle) == 180:
-  print(180)
-elif h_angle == m_angle:
-  print(0)
-else:
-  print(abs(h_angle - m_angle) % 180)
+print(min([abs(h_angle - m_angle), 360 - abs(h_angle - m_angle)]))
+# if abs(h_angle - m_angle) == 180:
+#   print(180)
+# elif h_angle == m_angle:
+#   print(0)
+# else:
+#   print(min([abs(h_angle - m_angle), 360 - abs(h_angle - m_angle)]))
