a0fdb74e7c512e3e7e002ac807ec136f0cfb84c5
totosuki <e231kari@gmail.com>
2023-07-05 13:40:34 +0900

    ABC 188 Bを修正

--- AtCoder/totosuki/ABC/B/188.py
+++ AtCoder/totosuki/ABC/B/188.py
@@ -2,8 +2,8 @@ import sys
 input = sys.stdin.buffer.readline
 
 N = int(input())
-A = tuple(map(int, input().split()))
-B = tuple(map(int, input().split()))
+A = map(int, input().split())
+B = map(int, input().split())
 rslt = int()
 
 for a,b in zip(A, B):
