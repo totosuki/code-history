f0833410d5824af8f953739995618fa26c125d05
totosuki <e231kari@gmail.com>
2023-07-06 17:59:33 +0900

    ABC 220 B Fix oneliner

--- AtCoder/totosuki/ABC/B/220.py
+++ AtCoder/totosuki/ABC/B/220.py
@@ -4,4 +4,9 @@ input = sys.stdin.buffer.readline
 K = int(input())
 A, B = map(lambda x: int(x, K), input().split())
 
-print(A * B)
+A,B=map(lambda x:int(x,int(input())),input().split())
+print(A*B)
+
+i=input;K=int(i());A,B=i().split();print(int(A,K)*int(B,K))
+
+j,i=int,input;K=j(i());A,B=i().split();print(j(A,K)*j(B,K))
