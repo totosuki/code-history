a156ff2792f85a396f46310588b750ee04e4d9a9
totosuki <e231kari@gmail.com>
2023-07-06 13:39:44 +0900

    ABC 014 A Fix oneliner

--- AtCoder/totosuki/ABC/A/014.py
+++ AtCoder/totosuki/ABC/A/014.py
@@ -21,6 +21,16 @@ for i in [[int(input()) for _ in [1,1]]]:print(i[1]-n if (n := i[0]%i[1]) else 0
 
 for i in[[int(input())for _ in[1,1]]]:print(i[1]-n if(n:=i[0]%i[1])else 0)
 
+for i in[[int(input())for _ in[1,1]]]:print((i[1]-i[0]%i[1])%i[1])
+
+print(b-a%b if(a:=int(input()))%(b:=int(input()))else 0)
+
+print((a:=int(input()))%(b:=int(input()))and b-a%b or 0)
+
+x,y=int(input()),int(input());print((y-x%y)%y)
+
+x,y=int(input()),int(input());print(-x%y%y)
+
 a=int(input());b=int(input());print(b-(a%b)if a%b else 0) # 違法ワンライナー
 
 
