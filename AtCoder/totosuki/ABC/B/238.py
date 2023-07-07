d010cbb27c3a67f7d0188e7e2560903524b018f2
totosuki <e231kari@gmail.com>
2023-07-07 13:25:53 +0900

    ABC 238 B Code Golf

--- AtCoder/totosuki/ABC/B/238.py
+++ AtCoder/totosuki/ABC/B/238.py
@@ -1,26 +1,36 @@
-import sys
-input = sys.stdin.buffer.readline
+_,A,L,m=input(),map(int,input().split()),[1]+[0]*359,{0}
+r=c=0
+for a in A:r,L[r]=(r+a)%360,1
+print(max([(c:=c*(-l+1)+1)for l in L]))
 
-N = int(input())
-A = list(map(int, input().split()))
-angle = 0
-L = [False] * 360
-L[0] = True
-rslt = list()
+# _,A,L,m=input(),map(int,input().split()),[1]+[0]*359,{0}
+# r=c=0
+# for a in A:r,L[r]=(r+a)%360,1
+# print(max([(c:=1)if l else(c:=c+1)for l in L]))
 
-for a in A:
-  angle += a
-  angle %= 360
-  L[angle] = True
+# import sys
+# input = sys.stdin.buffer.readline
 
-cnt = 0
-for i in range(360):
-  cnt += 1
-  if L[i] == True:
-    rslt.append(cnt)
-    cnt = 0
-else:
-  cnt += 1
-  rslt.append(cnt)
+# N = int(input())
+# A = list(map(int, input().split()))
+# angle = 0
+# L = [False] * 360
+# L[0] = True
+# rslt = list()
 
-print(max(rslt))
+# for a in A:
+#   angle += a
+#   angle %= 360
+#   L[angle] = True
+
+# cnt = 0
+# for i in range(360):
+#   cnt += 1
+#   if L[i] == True:
+#     rslt.append(cnt)
+#     cnt = 0
+# else:
+#   cnt += 1
+#   rslt.append(cnt)
+
+# print(max(rslt))
