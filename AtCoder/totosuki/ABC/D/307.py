89fa30ef6fe30cb0a1ff3102e7876e1954b64103
totosuki <e231kari@gmail.com>
2023-07-12 16:06:14 +0900

    ABC 307 D AC

--- AtCoder/totosuki/ABC/D/307.py
+++ AtCoder/totosuki/ABC/D/307.py
@@ -1,13 +1,25 @@
+import sys, collections
+input = sys.stdin.buffer.readline
+
 N = int(input())
-S = input()
-l = []
-r = []
+S = input().decode().strip()
+l_cnt = 0
+
+queue = collections.deque()
+
 for i in range(N):
   if S[i] == "(":
-    l.append(i)
-  if S[i] == ")" and l:
-    r.append[i]
+    l_cnt += 1
+    queue.append(S[i])
+  elif S[i] == ")":
+    if l_cnt > 0:
+      while True:
+        if queue.pop() == "(":
+          l_cnt -= 1
+          break
+    else:
+      queue.append(S[i])
+  else:
+    queue.append(S[i])
 
-tmp = min([len(l), len(r)])
-
-prnt(S[:])
+print("".join(queue))
