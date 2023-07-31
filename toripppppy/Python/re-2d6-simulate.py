025d8d6ecebe68e587a2e13806f89ee1912c5eaf
toripppppy <Lundy11@icloud.com>
2023-07-31 11:12:54 +0900

    おちゃの2d6シミュレーターをリファクタリング

--- toripppppy/Python/re-2d6-simulate.py
+++ toripppppy/Python/re-2d6-simulate.py
@@ -1,29 +1,50 @@
 import random #ramdomモジュールをインポート
 import time #timeモジュールをインポート
 
-trials = 10000000 #試行回数
-result = [0,0,0,0,0,0,0,0,0,0,0] #2から12までの配列
-timeformat = '%Y/%m/%d %H:%M:%S' #YYYY/MM/DD 24時間表記hh:mm:ss
 
-start = time.time() #開始時刻
+TRIALS = 10000000 #試行回数
+TIME_FORMAT = '%Y/%m/%d %H:%M:%S' #YYYY/MM/DD 24時間表記hh:mm:ss
 
-for num in range(trials): #決めた試行回数分繰り返す
-    x = random.randint(1,6) #ダイス1個目
-    y = random.randint(1,6) #ダイス2個目
-    z = x + y #二つのダイスの和
-    if num % 1000000 == 0: #もし百万の倍数なら
-        n = num // 1000000 #n百万を求める
-        print ('なう' + str(n) + '百万') #n百万をお知らせする
-    result[z-2] = result[z-2] + 1 #2から12を表す配列なので、2を引くことで2から配列を埋める
-print(result) #結果をプリント
+result = dict(zip(range(2, 13), [0]*11)) #2から12までのdict
 
-end = time.time() #終了時刻
-time_diff = end - start #終了時刻から開始時刻を引いて、処理にかかった時間を求める
 
-starttime = time.localtime(start) #UNIX時間をローカル時間に変換
-endtime = time.localtime(end) #UNIX時間をローカル時間に変換
+def get_2d6():
+    # 2d6の結果を返す
+    d1 = random.randint(1,6)
+    d2 = random.randint(1,6)
+    return d1 + d2
 
-starttimeymdhms = time.strftime(timeformat , starttime) #struct_time形式になっている時間を変換
-endtimeymdhms = time.strftime(timeformat , endtime) #struct_time形式になっている時間を変換
 
-print('開始時刻' + str(starttimeymdhms) + ',' + '終了時刻' + str(endtimeymdhms) + ',' + '処理時間' + str(time_diff)) #開始時刻、終了時刻、処理時間をそれぞれプリント
+def unix_to_str(unix_time: float):
+    # UNIX時間から文字列にフォーマット
+    struct_time = time.localtime(unix_time)
+    ymdhms = time.strftime(TIME_FORMAT , struct_time)
+    return ymdhms
+
+
+### 計測開始
+start = time.time()
+
+for i in range(TRIALS): # 決めた試行回数分繰り返す
+    d_sum = get_2d6() # 2d6を取得する
+    result[d_sum] += 1 # 取得結果に応じて集計
+
+    if i % 1000000 == 0: #もし百万の倍数なら
+        n = i // 1000000 #n百万を求める
+        print (f'なう{n}百万') #n百万をお知らせする
+
+print(f'試行結果: {result}') #結果を出力
+
+### 計測終了
+end = time.time()
+
+
+# 計測開始・終了時間を文字列にフォーマット
+start_ymdhms = unix_to_str(start)
+end_ymdhms = unix_to_str(end)
+
+# 終了時刻から開始時刻を引いて、処理にかかった時間を求める
+time_diff = end - start
+
+# 開始時刻、終了時刻、処理時間をそれぞれ出力
+print(f'開始時刻: {start_ymdhms}\n終了時刻: {end_ymdhms}\n処理時間: {time_diff}')
