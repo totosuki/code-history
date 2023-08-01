31f8379b4ed36f780111f86837d5a43b4ecb5796
totosuki <e231kari@gmail.com>
2023-08-01 17:54:39 +0900

    get_date関数を追加

--- totosuki/Python/ABCNanika-Scraper/main.py
+++ totosuki/Python/ABCNanika-Scraper/main.py
@@ -11,6 +11,7 @@ url = arg[1]
 PROBLEMS = "?activeTab=Problems"
 STANDINGS = "?activeTab=Standings"
 URL = url.replace(STANDINGS,"").replace(PROBLEMS, "")
+DATE_PATH = "//table[@class='mb-0 table']/tbody/tr/td"
 PROB_URL_PATH = "//table[@class='table table-sm table-bordered table-striped']/tbody/tr/td/a[@href]"
 
 def start():
@@ -18,6 +19,13 @@ def start():
   driver = webdriver.Chrome(options=options)
   driver.get(URL + PROBLEMS)
   return driver
+
+def get_date(driver: webdriver.Chrome) -> str:
+  elem = driver.find_element(By.XPATH, DATE_PATH)
+  value = elem.text
+  
+  match = re.search(r'(\d{4}-\d{2}-\d{2})', value)
+  return match.group(1)
 
 def get_number(driver: webdriver.Chrome) -> int:
   elem = driver.find_element(By.XPATH, PROB_URL_PATH)
@@ -37,7 +45,9 @@ def get_number(driver: webdriver.Chrome) -> int:
 def main():
   driver = start()
   time.sleep(5)
+  date = get_date(driver)
   number = get_number(driver)
+  print(date)
   print(number)
 
 if __name__ == "__main__":
