10470c620e419941d6f04d0f4736a9cc8d7eef44
totosuki <e231kari@gmail.com>
2023-08-01 14:55:35 +0900

    ABC C 312 AC

--- AtCoder/totosuki/ABC/C/312.py
+++ AtCoder/totosuki/ABC/C/312.py
@@ -4,40 +4,32 @@ input = sys.stdin.buffer.readline
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-A.sort()
-B.sort()
-R = [0] * N
-rslt = 0
 
-for i in range(N):
-  if i == 0:
-    if B[0] < A[0]:
-      R[i] = 1
-    else:
-      R[i] = 0
-  else:
-    R[i] = R[i-1]
+left = -1
+right = 10**9+5
+
+while (right - left) > 1:
+  mid = (right + left) // 2
+  buy_cnt = 0
+  sell_cnt = 0
   
-  while R[i] < M and B[R[i]] < A[i]:
-    R[i] += 1
-
-print(R)
-
-for i in range(N):
-  if i+1 >= M - R[i] and M - R[i] != 0:
-    if R[i] <= i:
-      print(B[R[i]-1])
-    else:
-      print(A[i])
-    break
-else:
-  if R[0] != 0:
-    print(B[-1] + 1)
+  for i in range(N):
+    if A[i] <= mid:
+      buy_cnt += 1
+  for i in range(M):
+    if B[i] >= mid:
+      sell_cnt += 1
+  
+  if buy_cnt >= sell_cnt:
+    right = mid
   else:
-    print(B[M - N - 1] + 1)
+    left = mid
 
+print(right)
 
-# case
-# 3 4
-# 500 500 500
-# 100 100 100 100
+"""
+x x x x o o o o
+      ^ ^
+      l r
+だからrをoutput
+"""
