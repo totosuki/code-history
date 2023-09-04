e8b37977f21e5f033d8f05c0bec406a255583ffa
totosuki <e231kari@gmail.com>
2023-09-04 13:48:43 +0900

    ABC 305 D 解説AC

--- AtCoder/totosuki/ABC/D/305.py
+++ AtCoder/totosuki/ABC/D/305.py
@@ -1,56 +1,45 @@
-def binary_search_start(x, y):
-  min = 0
-  max = len(x)-1
-  while min <= max:
-    middle = (min+max) // 2
-    if x[middle-1] <= y <= x[middle]:
-      return middle
-    elif x[middle] < y:
-      min = middle + 1
-    else:
-      max = middle - 1
-  return middle
-
-def binary_search_end(x, y):
-  min = 0
-  max = len(x)-1
-  while min <= max:
-    middle = (min+max) // 2
-    if x[middle - 1] <= y <= x[middle]:
-      return middle
-    elif x[middle] < y:
-      min = middle + 1
-    else:
-      max = middle - 1
-  return middle
-
-def convert_1d_to_2d(l, cols):
-  return [l[i:i + cols] for i in range(0, len(l), cols)]
+import sys
+input = sys.stdin.buffer.readline
 
 N = int(input())
 A = list(map(int, input().split()))
-clean_A = convert_1d_to_2d(A[1:], 2)
 Q = int(input())
-all_data = [list(map(int, input().split())) for _ in range(Q)]
-for data in all_data:
-  start = binary_search_start(A, data[0])
-  end = binary_search_end(A, data[1])
-  clean_A = clean_A[start//2-1:end//2]
-  print(f"data: {data} |||| {clean_A}")
-  rslt = 0
-  for d in clean_A:
-    rslt += d[1] - d[0]
-  if start % 2 == 0:
-    rslt -= data[0] - clean_A[0][0]
-  if end % 2 == 0:
-    rslt -= clean_A[-1][1] - data[1]
-  if start % 2 == 1:
-    if start == clean_A[0][1]:
-      print("aa")
-      rslt -= clean_A[0][1] - clean_A[0][0]
-  print(rslt)
 
+def binary_search(x):
+  left = -1
+  right = N
+  
+  while right - left > 1:
+    mid = (left + right) // 2
+    if A[mid] <= x:
+      left = mid
+    else:
+      right = mid
 
+  return left
 
+# 累積和
+S = [0] * N
+for i in range(1, N):
+  S[i] = S[i-1]
+  if i % 2 == 0:
+    S[i] += A[i] - A[i-1]
 
-
+for _ in range(Q):
+  left, right = -1, N
+  l, r = map(int, input().split())
+  
+  start = binary_search(l)
+  end = binary_search(r)
+  
+  if start % 2:
+    start_sm = S[start] + (l - A[start])
+  else:
+    start_sm = S[start]
+  
+  if end % 2:
+    end_sm = S[end] + (r - A[end])
+  else:
+    end_sm = S[end]
+  
+  print(end_sm - start_sm)
