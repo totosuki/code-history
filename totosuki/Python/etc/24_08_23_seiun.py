73444a15ee070b8dc4445420303a9c93590d9faf
totosuki <e231kari@gmail.com>
2024-08-24 01:12:36 +0900

    色チェックシステムの完成

--- totosuki/Python/etc/24_08_23_seiun.py
+++ totosuki/Python/etc/24_08_23_seiun.py
@@ -36,8 +36,6 @@ except:
   print("The number of loop times must be entered as a command line argument.")
   exit()
 
-min = Axis(0, 0)
-max = Axis(1440, 900)
 update = Axis(980, 730)
 update_yes = Axis(900, 585)
 diff = Axis(90, -145)
@@ -49,17 +47,21 @@ pack1_buy = Axis(pack1.x + diff.x, pack1.y + diff.y)
 pack2_buy = Axis(pack2.x + diff.x, pack2.y + diff.y)
 pack3_buy = Axis(pack3.x + diff.x, pack3.y + diff.y)
 pack4_buy = Axis(pack4.x + diff.x, pack4.y + diff.y)
-choose = Axis(750, 720)
-itemlast = Axis(1350, 400)
-sell = Axis(1030, 675)
-
+choose1 = Axis(750, 720)
+choose2 = Axis(1035, 720)
+choose3 = Axis(1315, 720)
+chooses = [choose1, choose2, choose3]
 choose1_color = Axis(790, 540)
 choose2_color = Axis(1080, 540)
 choose3_color = Axis(1360, 540)
+choose_colors = [choose1_color, choose2_color, choose3_color]
+itemlast = Axis(1350, 400)
+sell = Axis(1030, 675)
 
 gold = Color(89, 64, 36)
 blue = Color(62, 76, 98)
 green = Color(63, 84, 67)
+colors = [green, blue, gold]
 
 positions = {
   update,
@@ -72,7 +74,9 @@ positions = {
   pack2_buy,
   pack3_buy,
   pack4_buy,
-  choose,
+  choose1,
+  choose2,
+  choose3,
   itemlast,
   sell
 }
@@ -87,6 +91,15 @@ def click(x, y, waittime = 1):
   check_stop(*pag.position())
   pag.click(x, y)
 
+def choose_pack():
+  time.sleep(2)
+  rank = [] # gold : 2, blue : 1, green : 0
+  for pos in choose_colors:
+    color = Color(*pag.pixel(pos.x * 2, pos.y * 2))
+    rank.append(colors.index(color))
+  choose = chooses[rank.index(max(rank))]
+  click(choose.x, choose.y)
+
 def main(N):
   pag.click(update.x, update.y)
   for _ in range(N):
@@ -95,7 +108,7 @@ def main(N):
     for pack in [pack1, pack2, pack3, pack4]:
       click(pack.x, pack.y)
       click(pack.x + diff.x, pack.y + diff.y, 0.5)
-      click(choose.x, choose.y, 1.5)
+      choose_pack()
       click(itemlast.x, itemlast.y)
       click(sell.x, sell.y, 0.5)
 
